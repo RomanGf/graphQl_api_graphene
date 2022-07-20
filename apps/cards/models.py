3aebc769180469af63772dbf5cc27708f668d967
RomanGf <gafyak.roma@gmail.com>
2022-07-21 00:51:54 +0300

    build graphQl api

---
+++ apps/cards/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+from django.utils import timezone
+
+from apps.decks.models import Deck
+from apps.utils.models import Timestamps
+# Create your models here.
+
+class Card(Timestamps):
+    deck = models.ForeignKey(Deck, on_delete=models.CASCADE)
+    question = models.TextField()
+    answer = models.TextField()
+    buckets = (
+        (1, '1 Day'),
+        (2, '3 Day'),
+        (3, '7 Day'),
+        (4, '16 Day'),
+        (5, '30 Day'),
+    )
+    bucket = models.IntegerField(choices=buckets, default=1)
+    next_review_at = models.DateTimeField(default=timezone.now())
+    last_review_at = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.question
+
